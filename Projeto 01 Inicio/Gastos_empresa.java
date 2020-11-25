6084b2eb0359c824d1c8148a939eb3bc5fda4b19
Camila Belmont <camilabelmont.alu@gmail.com>
2020-11-25 09:05:08 -0300

    Mudando estrutura do codigo

--- Projeto 01 Inicio/Gastos_empresa.java
+++ Projeto 01 Inicio/Gastos_empresa.java
@@ -1,8 +1,6 @@
 class BalancoTrimestral {
 	public static void main(String[] args) {
-		int janeiro = 15000;
-		int fevereiro = 23000;
-		int marco = 17000;
+		int janeiro = 15000, fevereiro = 23000, marco = 17000;
 
 		int gastosTrimestre = janeiro + fevereiro + marco;
 
