88b22d7391bb09a0cc46d1ae13a660947cf1a2c6
Camila Belmont <camilabelmont.alu@gmail.com>
2020-11-24 22:00:47 -0300

    Criando novo código e alterando nomes

---
+++ Gastos_empresa.java
@@ -0,0 +1,11 @@
+class BalancoTrimestral {
+	public static void main(String[] args) {
+		int janeiro = 15000;
+		int fevereiro = 23000;
+		int marco = 17000;
+
+		int gastosTrimestre = janeiro + fevereiro + marco;
+
+		System.out.println(gastosTrimestre);
+	}
+}
