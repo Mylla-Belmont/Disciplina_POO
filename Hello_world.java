7af0c4bdcc70cb7dd03b614011beec3715bd0fd1
Camila Belmont <camilabelmont.alu@gmail.com>
2020-11-25 09:26:00 -0300

    Resolvendo problemas de sintax

--- Hello_world.java
+++ Hello_world.java
@@ -1,7 +1,5 @@
-import static java.lang.System.out;
-
 class testeJava {
     public static void main(String[] args) {
-        out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
