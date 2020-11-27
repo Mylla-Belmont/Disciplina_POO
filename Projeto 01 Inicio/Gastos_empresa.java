76367d47670a32cd68771b3426dda23977397e22
Camila Belmont <71456854+Mylla-Belmont@users.noreply.github.com>
2020-11-27 00:18:54 -0300

    Delete Gastos_empresa.java

--- Projeto 01 Inicio/Gastos_empresa.java
+++
@@ -1,9 +0,0 @@
-class BalancoTrimestral {
-	public static void main(String[] args) {
-		int janeiro = 15000, fevereiro = 23000, marco = 17000;
-
-		int gastosTrimestre = janeiro + fevereiro + marco;
-
-		System.out.println(gastosTrimestre);
-	}
-}
