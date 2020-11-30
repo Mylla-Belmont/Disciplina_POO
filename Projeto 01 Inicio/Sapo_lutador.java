92b86ea7b302bad9d2afe34a6af8d4490ec1a410
Camila Belmont <camilabelmont.alu@gmail.com>
2020-11-30 11:22:14 -0300

    Novo

--- Projeto 01 Inicio/Sapo_lutador.java
+++ Projeto 01 Inicio/Sapo_lutador.java
@@ -1,1 +1,13 @@
-xxx
+import java.util.Scanner;
+
+public class Sapo_lutador{
+    public static void main(String[] args){
+        Sapo_lutador Kermit = new Sapo_lutador();
+        Sapo_lutador Saiyajin = new Sapo_lutador();
+
+        Kermit.atacar();
+        Kermit.curar();
+        Kermit.recuperarPoder();
+        Kermit.dano();
+    }
+}
