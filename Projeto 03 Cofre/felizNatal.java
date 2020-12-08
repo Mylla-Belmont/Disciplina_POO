c802d28fb5bd7c69570dcad98eff275acd8ac3f5
Camila Belmont <camilabelmont.alu@gmail.com>
2020-12-08 16:41:42 -0300

    Criando estrutura do código

---
+++ Projeto 03 Cofre/felizNatal.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+
+class Personagens  {
+    int vida;
+    
+    Personagens(int vida){
+        this.vida = vida;
+    }
+
+    void perderVida(){
+        vida -= golpe;
+    }
+
+    int atacar(){
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+        Personagens papaiNoel = new Personagens(0);
+        Personagens doidin = new Personagens(0);
+
+        System.out.println(papaiNoel);
+        System.out.println(doidin);
+    }
+}
+
+public class felizNatal {
+
+    public static void main(String[] args) {
+
+        
+
+    }
+}
