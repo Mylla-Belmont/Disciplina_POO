82ad2a73485993ca8652a1c9e3ea5eb8ab1a3147
Camila Belmont <camilabelmont.alu@gmail.com>
2021-01-01 15:03:37 -0300

    Update

--- Projeto 05 contato/agenda.java
+++ Projeto 05 contato/agenda.java
@@ -1,17 +1,57 @@
-class contato{
-    public static void main(String[] args) {
-        
+import java.util.Scanner;
+import java.util.ArrayList;
+
+class fone{
+    
+    String label;
+    String number;
+
+    fone(String label, String number){
+        this.label = label;
+        this.number = number;
+    }
+
+    public boolean validandoNumero(String number){      
+        String validos = "0123456789()-";
+
+        for(int i=0; i < number.length(); i++)              //Percorre o vetor de acordo com o seu tamanho /number.length()/
+            if(validos.indexOf(number.charAt(i)) == -1)     // .indexOf - Retorna a posição da primeira ocorrencia de um caracter especifico
+                return false;                               // .charAt() - retorna caracter especificado da posição i
+        return true;
+    }
+
+    public String toString(){
+        return label + ":" + number;
     }
 }
 
-class fone{
-    public static void main(String[] args) {
-        
+class contato{
+
+    private String nome;
+    ArrayList<Fone> fones; 
+
+    contato(String nome){
+        this.nome = nome;
+        this.fones = new ArrayList();
     }
+
+    private void setAdicionarFone(int label, int number){
+        fones.add(label, number);    
+    }
+
+    private void getFones(){
+        return fones;
+    }
+
+    public static void main(String[] args) {
+        }
 }
+
 
 public class agenda {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
         
     }
 }
