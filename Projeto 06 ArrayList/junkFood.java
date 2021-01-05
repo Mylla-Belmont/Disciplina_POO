be0fb9987a6c90f7a6a7e0eda031e0ac405b36a6
Camila Belmont <camilabelmont.alu@gmail.com>
2021-01-04 23:49:25 -0300

    Update

--- Projeto 06 ArrayList/junkFood.java
+++ Projeto 06 ArrayList/junkFood.java
@@ -22,31 +22,38 @@ class Espiral{
 
 class junkFood{
 
-    int maxProdutos;
-    float saldoCliente;
     float lucro;
+    float saldoCliente;
+    int maxProdutos;
     ArrayList<Espiral> espiral;
 
-    public junkFood(int numEspirais, int maxProdutos, float saldoCliente, float lucro){
-        this.saldoCliente = saldoCliente;
-        this.lucro = lucro;
+    junkFood(int numEspirais, int maxProdutos){
+        this.lucro = 0;
+        this.saldoCliente = 0;
         this.maxProdutos = maxProdutos;
         this.espiral = new ArrayList<>();
-        for(int i = 0; i < numEspirais; i++)
-            this.espiral.add(new Espiral("", 0, 0f));
+        for(int i=0; i < numEspirais; i++)
+            this.espiral.add(new Espiral("-", 0, 0));
+    }
+
+    void alterarEspiral(int index, String nome){
+        if(index >= 0 && index < espiral.size())
+            espiral.set(index, nome); 
     }
 
     public String toString(){
-        String output = "";
+        String saida = "";
         int i = 0;
         for(Espiral espiral : espiral){
-            output += i + " [ " + espiral + " ]\n";
+            saida += " [" + i + ":" + espiral + "]";
             i++;
         }
-        return output;
-    }   
+        return saida;
+    }
 
-    public static void main (String[] args){
-        junkFood maquina = new junkFood(0, 0 , 0 , 0);
+    public static void main(String[] args){
+        junkFood maquina = new junkFood(3, 5);
+
+        maquina.alterarEspiral(1, "nome");
     }
 }
