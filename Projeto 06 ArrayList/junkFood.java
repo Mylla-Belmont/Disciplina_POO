f44fd3f2b4f3c0e62646cec2c3f2eedb533f444d
Camila Belmont <camilabelmont.alu@gmail.com>
2021-01-05 15:30:32 -0300

    Update

--- Projeto 06 ArrayList/junkFood.java
+++ Projeto 06 ArrayList/junkFood.java
@@ -16,7 +16,7 @@ class Espiral{
         this.nome = nome;
         this.quantidade = quantidade;
         this.preco = preco;
-    }       
+    }     
 }
 
 
@@ -36,16 +36,47 @@ class junkFood{
             this.espiral.add(new Espiral("-", 0, 0));
     }
 
-    void alterarEspiral(int index, String produto, int qtd, float preço){
+
+    float pedirTroco(){
+        return saldoCliente;
+    }
+
+    void vender(int index){
+        if(saldoCliente > 0){
+            float valorProduto = espiral.get(index).preco;
+            saldoCliente -= valorProduto;
+            espiral.get(index).quantidade -= 1;
+            
+            if(saldoCliente < 0)
+                saldoCliente = 0;
+            
+            if(espiral.get(index).quantidade < 0)
+                espiral.get(index).quantidade = 0;
+        }else
+            System.out.println("Saldo insuficiente");
+    }
+
+    void inserirDinheiro(float dinheiro){
+        saldoCliente += dinheiro; 
+    }
+
+    void limparEspiral(int index){
+        if(index >= 0 && index < espiral.size()){
+            espiral.remove(index);
+            espiral.add(index, new Espiral("-", 0, 0));
+        }
+    }
+
+    void alterarEspiral(int index, String produto, int quantidade, float preco){
         if(index >= 0 && index < espiral.size())
-            espiral.set(index, new Espiral(produto, qtd, preço)); 
+            espiral.set(index, new Espiral(produto, quantidade, preco)); 
     }
 
     public String toString(){
-        String saida = "";
+        String saida = "Saldo: " + saldoCliente + "\n";
         int i = 0;
         for(Espiral espiral : espiral){
-            saida += i + " [ " + espiral.nome + " : " + espiral.quantidade + " U : " + espiral.preco + " RS]\n";
+            saida +=  i + " [" + espiral.nome + " : " + espiral.quantidade + " U : " + espiral.preco + " RS]\n";
             i++;
         }
         return saida;
@@ -54,7 +85,28 @@ class junkFood{
     public static void main(String[] args){
         junkFood maquina = new junkFood(3, 5);
 
-        maquina.alterarEspiral(2, "todinho", 3, 2.50f); 
+        //Inserindo produtos na espirais
+        maquina.alterarEspiral(0, "picanha", 2, 1.50f);
+        maquina.alterarEspiral(1, "todinho", 3, 2.50f); 
+        maquina.alterarEspiral(2, "pamonha", 5, 5.20f); 
+
+        //Removendo produtos na espirais
+        //maquina.limparEspiral(1);
+
+        //Inserindo dinheiro
+        maquina.inserirDinheiro(5f);
+        maquina.inserirDinheiro(5f);
+
+        //Pedir troco
+        maquina.pedirTroco();
+
+        //Vender produto
+        maquina.vender(1);
+        maquina.vender(1);
+        maquina.vender(1);
+        maquina.vender(1);
+
+
         System.out.println(maquina);
 
     }
