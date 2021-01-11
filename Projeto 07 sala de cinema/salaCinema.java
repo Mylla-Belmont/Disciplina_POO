29384d79be68462406fd7507448f8e3c6db8e0c8
Camila Belmont <camilabelmont.alu@gmail.com>
2021-01-11 08:40:00 -0300

    Update code

--- Projeto 07 sala de cinema/salaCinema.java
+++ Projeto 07 sala de cinema/salaCinema.java
@@ -1,1 +1,50 @@
-****
+import java.util.ArrayList;
+
+
+class Cliente{
+
+    String identificador;
+    String fone;
+
+    Cliente(String identificador, String fone){
+        this.identificador = identificador;
+        this.fone = fone;
+    }
+
+    public void cliente(String identificador, String fone){
+        this.identificador = identificador;
+        this.fone = fone;
+    }
+}
+
+class cinema{
+
+    ArrayList<Cliente> cliente;
+
+    cinema(int lugares){
+        this.cliente = new ArrayList<>();
+        for(int i=0; i < lugares; i++)
+            this.cliente.add(new Cliente("-", " "));
+    }
+
+    void reservar(String identificador, String fone, int index){
+        cliente.set(index, new Cliente(identificador, fone));
+    }  
+    
+    void cancelar(int index){
+        cliente.set(index, new Cliente("-", " "));
+    }
+    
+    public String toString(){
+        for(Cliente cliente : cliente){
+            return  " [" + cliente.identificador + " : " + cliente.fone;
+        }
+        return " ";
+    }
+}
+
+public class salaCinema{
+    public static void main(String[] args){
+
+    }
+}
