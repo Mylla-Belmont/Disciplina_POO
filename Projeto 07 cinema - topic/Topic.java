2b72ee9bdcbe8c913a0fdc96e6d2bfa06d0f9740
Camila Belmont <camilabelmont.alu@gmail.com>
2021-01-16 15:18:56 -0300

    Update

--- Projeto 07 cinema - topic/Topic.java
+++ Projeto 07 cinema - topic/Topic.java
@@ -23,6 +23,7 @@ class Carro{
     Carro(int qtd, int qtdPreferencial){
         this.qtdPreferencial = qtdPreferencial;
         this.cadeiras = new ArrayList<>();
+
         for(int i=0; i < qtd; i++)
             cadeiras.add(null);
     }
@@ -31,31 +32,47 @@ class Carro{
     //     return true;
     // }
 
-    // boolean subir(Passageiros passageiro){
-    //     return true;
-    // }
+    void subir(String nome, int idade){
+        int i = 0;
+        for(Passageiros passageiro : cadeiras){
+            if(passageiro.idade >= 60 && qtdPreferencial != 0 && cadeiras.get(i) == null){
+                cadeiras.set(i, new Passageiros(nome, idade));
+                qtdPreferencial -= 1;
+                i++;
+            }
+        }
+    }
 
-    public String ToString(){
+    public String toString(){
         String saida = "";
-        for(Passageiros cadeiras : cadeiras)
-        
-            for(int i=0; i < qtdPreferencial; i++)
-                if(cadeiras == null)
-                    saida += " @ ";
+        int preferencial = qtdPreferencial;
 
-            if(cadeiras == null)
+        for(Passageiros cadeiras : cadeiras){
+            if(preferencial != 0 && cadeiras == null){
+                saida += " @ ";
+                preferencial -= 1;
+            }
+            if(preferencial == 0 && cadeiras == null)
                 saida += " = ";
-
-        return "'[" + saida + "]'";
+        }
+        return "[" + saida + "]";
     }
 }
 
 public class Topic{
-
     public static void main(String[] args){
 
         Carro carro = new Carro(5, 2);
 
+        carro.subir("davi", 17);
+        carro.subir("joão", 103);
+
         System.out.println(carro);
     }
 }
+
+
+// for(int i=0; i < qtdPreferencial; i++){
+//     if(cadeiras == null)
+//         saida += " @ ";
+// }
