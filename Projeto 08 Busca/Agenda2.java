d75152c2d6292b0928e8f830cd7a124752f76728
Camila Belmont <camilabelmont.alu@gmail.com>
2021-01-24 18:21:25 -0300

    Update

--- Projeto 08 Busca/Agenda2.java
+++ Projeto 08 Busca/Agenda2.java
@@ -91,9 +91,10 @@ class Agenda{
     //     for(int i=0; i < fone.size(); i++)
     //         number += fone.get(i);
 
-    //     for(Contato contato : contato)
-    //         this.contato.add(contato.addFone(name, number));
-    // }
+        // // for(Contato contato : contato)
+        //     contato.addAll(con)
+        //     this.contato.add(contato.addFone(name, number));
+    }
 
     // void rmContato(String name){
     //     for(int i=0; i < contato.size(); i++)
