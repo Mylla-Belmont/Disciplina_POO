d320307bcd948b43ebbd5f06e4db815dc6de41e0
Camila Belmont <camilabelmont.alu@gmail.com>
2021-01-26 19:13:59 -0300

    Update

--- Projeto 08 Busca/Agenda2.java
+++ Projeto 08 Busca/Agenda2.java
@@ -3,10 +3,10 @@ import java.util.List;
 import java.util.Arrays;
 
 class Fone{
-    String id;
-    String number;
+    String[] id;
+    String[] number;
 
-    Fone(String id, String number){
+    public Fone(String[] id, String[] number){
         this.id = id;
         this.number = number;
     }
@@ -17,7 +17,7 @@ class Fone{
         for(int i=0; i < number.length(); i++)
             if(validos.indexOf(number.charAt(i)) == -1)
                 return false;
-        return true;
+            return true;
     }
 
     public String toString(){
@@ -29,38 +29,18 @@ class Contato{
     String name;
     ArrayList<Fone> fones;
 
-    Contato(String name){
+    public Contato(String name){
         this.name = name;
         this.fones = new ArrayList<>();
     }
 
-    void addFone(String id, String number){
-        if(Fone.validate(number))
-            fones.add(new Fone(id, number));
-        System.out.println("Número inválido");
+    void addFone(String[] id, String[] number){
+        for(int i=0; i < id.length; i++)
+            if(Fone.validate(number[i]))
+                fones.add(new Fone(id, number));
+            else
+                System.out.println("Número inválido");
     }
-
-    // Fone getFone(int index){
-    //     if(index < 0 && index > fones.size()){
-    //         System.out.println("Número não encontrado");
-    //         return null;
-    //     }
-    //     return fones.get(index);
-    // }
-
-    // Fone getName(String numero){
-    //     for(Fone fone : fones)
-    //         if(fone.number.equals(numero))
-    //             return fone;
-    //     return null;
-    // }
-
-    // // void rmFone(String name){
-    //     for(Fone fone : fones)
-    //         if(fone.id.equals(name))
-    //             fones.remove();
-    //     System.out.println("O seguinte número não existe");
-    // }
 
     public String toString(){
         String saida = this.name;
@@ -76,7 +56,7 @@ class Contato{
 class Agenda{
     ArrayList<Contato> contato;
 
-    Agenda(){
+    public Agenda(){
         this.contato = new ArrayList<>();
     }
 
@@ -92,25 +72,15 @@ class Agenda{
             System.out.println("Esse contato já existe");
             return;
         }
-        
+
         String number = "";
-        for(int i=0; i < fone.size(); i++)
-            number += fone.get(i);    
-
-        for(Contato contato : contato)
-            contato.addFone(name, number);
+        for(int i=0; i < fone.size(); i++){
+            number += fone;
+            
+            for(Contato contato : contato)
+                this.contato.add(contato.addFone(name, number));
+        }
     }
-
-    // void rmContato(String name){
-    //     for(int i=0; i < contato.size(); i++)
-    //         if(contato.get(i).name.equals(name))
-    //             contato.remove(name);
-    //     System.out.println("O contato não existe");
-    // }
-    
-    // void removerFone(String number, int index){
-
-    // }
 
     public String toString(){
         String saida = "";
@@ -124,13 +94,13 @@ public class Agenda2{
     public static void main(String[] agrs){
 
         Agenda agenda = new Agenda();
-
+        
         //Adicionando contato
         agenda.addContato("eva", Arrays.asList(new Fone("oio", "8585"), new Fone("cla", "9999")));
         System.out.println(agenda);
 
-        // //Removendo contato
-        // agenda.rmFone("ana", 0);
-        // System.out.println(agenda);
+    //     //Removendo contato
+    //     agenda.rmFone("ana", 0);
+    //     System.out.println(agenda);
     }
 }
