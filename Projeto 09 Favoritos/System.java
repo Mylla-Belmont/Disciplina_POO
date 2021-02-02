edcf6784d146566cd68ae8f08822db9107ef718d
Camila Belmont <camilabelmont.alu@gmail.com>
2021-02-02 00:05:14 -0300

    Novo projeto

---
+++ Projeto 09 Favoritos/System.java
@@ -0,0 +1,78 @@
+import java.util.ArrayList;
+import java.util.TreeMap;
+
+class Fone{
+    String label;
+    String number;
+
+    Fone(String label, String number){
+        this.label = label;
+        this.number = number;
+    }
+
+    public static boolean validate(String number){
+        String validos = "123456789-()";
+        for(int i=0; i < number.length(); i++)
+            if(validos.indexOf(number.charAt(i)) == -1)
+                return true;
+        return false;
+    }
+
+    public String toString(){
+        return label + ":" + number;
+    }
+}
+
+class Contato{
+    String name;
+    ArrayList<Fone> fones;
+    boolean starred;
+
+    Contato(String name, boolean starred){
+        this.name = name;
+        this.starred = starred;
+        this.fones = new ArrayList<>();
+    }
+
+    void addFone(String label, String number){
+        if(Fone.validate(number)){
+            fones.add(new Fone(label, number));
+            return;
+        }
+        //Fazer uma excessão aqui
+    }
+
+    void rmFone(int index){
+        if(fones.get(index) != null){
+            fones.remove(index);
+            return;
+        }
+        //Fazer uma excessão aqui
+    }
+
+    public String toString(){
+        StringBuilder saida = new StringBuilder();
+        int i=0;
+        for (Fone fones : fones) {
+            saida.append("[" + i + ":" + fones + "]");
+            i++;
+        }
+        return saida.toString();
+    }
+}
+
+class Agenda{
+    TreeMap<String, Contato> contatos;
+    TreeMap<String, Contato> bookMarks;
+
+    Agenda(){
+        this.contatos = new TreeMap<>();
+        this.bookMarks = new TreeMap<>();
+    }
+}
+
+public class System{
+    public static void main(String[] agrs){
+
+    }
+}
