5c193a05842b1e5563f6e0a9e0dd8cf55cc190a9
Camila Belmont <camilabelmont.alu@gmail.com>
2021-02-15 18:02:13 -0300

    Update

--- Projeto 10 Batalha Gem/CampoDeBatalha.java/src/Code/Ametistas.java
+++ Projeto 10 Batalha Gem/CampoDeBatalha.java/src/Code/Ametistas.java
@@ -2,17 +2,17 @@ package Code;
 
 import java.util.ArrayList;
 
-public class Perolas extends Gems{    //
+public class Ametistas extends Gems{
     int poder;
     int maxPoder;
     int energia;
-    int minimoEnergia = 20;
+    int minimoEnergia = 10;
     int maximoRecuperação = 2;
     int resistenciaArma;
     boolean vida = true;
     ArrayList<Fusões> fusão;
 
-    Perolas(int poder, int energia, int resistenciaArma){
+    Ametistas(int poder, int energia, int resistenciaArma){
         this.poder = poder;
         this.maxPoder = poder;
         this.energia = energia;
@@ -20,45 +20,46 @@ public class Perolas extends Gems{    //
         this.fusão = new ArrayList<>();
     }
 
-    public int atacar(){
+    int atacar() {
         if(energia >= minimoEnergia){
-            poder -= 5;
-            energia -= 5;
-            resistenciaArma -= 5;
-            return 10;
-        }throw new RuntimeException("Perola está com a energia baixa!");
+            poder -= 10;
+            energia -= 10;
+            resistenciaArma -= 10;
+            return 5;
+        }throw new RuntimeException("Amestista está com a energia baixa!");
     }
 
-    public void sofrerDano(int dano){
+    void sofrerDano(int dano) {
         if(energia - dano < 0){
             vida = false;
             energia = 0;
-            throw new RuntimeException("Perola foi destruida!");
+            throw new RuntimeException("Amestista foi destruida!");
         }
         if(vida == true)
             energia -= dano;
     }
 
-    public int usarPoder(){
-        if(poder >= (maxPoder/2) && energia >= 20){
-            poder -= 10;
-            energia -= 10;
+    int usarPoder() {
+        if(poder >= (maxPoder/2) && energia >= 10){
+            poder -= 20;
+            energia -= 20;
             return maxPoder;
-        } throw new RuntimeException("Perola está fraca!");
+        } throw new RuntimeException("Ametista está fraca!");
     }
 
-    public void recuperarEnergia(){
+    void recuperarEnergia() {
         if(vida == true && maximoRecuperação != 0){
             poder += 20;
-            energia += 20;
-            resistenciaArma += 20;
+            energia += 5;
+            resistenciaArma += 10;
             maximoRecuperação -= 1;
-        }throw new RuntimeException("Perola está morta!");
-    }    
+        }throw new RuntimeException("Ametista está morta!");
 
-    public void fundir(String nomeFusão){
+    }
+
+    void fundir(String nomeFusão) {
         if(vida == true && energia >= minimoEnergia){
             fusão.add(new Fusões("Perola", nomeFusão));
         }throw new RuntimeException("Perola não pode fundir-se com" + nomeFusão);
-    }
+    }   
 }
